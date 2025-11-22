5288df9de9fbf6c661bf3345712975879844e028
tcarbonell24 <tcarbonell12@gmail.com>
2025-11-21 20:20:07 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Customer, CustomerSchema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -16,6 +16,20 @@ db.init_app(app)
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
 
+@app.route('/customers')
+def customers():
+    customers = Customer.query.all()
+    print(customers)
+    response_body = CustomerSchema().dumps(customers)
+    print(response_body)
+    return make_response(response_body)
+
+@app.route('/customers/<int:id>')
+def customer(id):
+    customer = Customer.query.filter(Customer.id == id).first()
+    response_body = CustomerSchema().dump(customer)
+    print(response_body)
+    return make_response(response_body)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -17,9 +17,17 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
+class CustomerSchema(Schema):
+    id = fields.Int()
+    name = fields.String()
+    
+    reviews = fields.List(fields.Nested(lambda: ReviewSchema))
 
 class Item(db.Model):
     __tablename__ = 'items'
@@ -28,5 +36,34 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class ItemSchema(Schema):
+    id = fields.Int()
+    name = fields.String()
+    price = fields.Float()
+
+    reviews = fields.List(fields.Nested(lambda: ReviewSchema))
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    item = db.relationship('Item', back_populates='reviews')
+    customer = db.relationship('Customer', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}, {self.customer_id}, {self.item_id}>'
+class ReviewSchema(Schema):
+    id = fields.Int()
+    comment = fields.String()
+
+    customer = fields.Nested(CustomerSchema(exclude=('reviews',)))
+    item = fields.Nested(ItemSchema(exclude=('reviews',)))
